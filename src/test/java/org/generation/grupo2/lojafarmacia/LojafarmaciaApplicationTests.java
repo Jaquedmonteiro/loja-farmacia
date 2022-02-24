1fe07b479b26802d4d4b22e59f7eb3c9fd1b4371
Jônatas <OliveiraJonatas@Outlook.com.br>
2022-02-24 10:53:23 -0300

    @jonatasoliveiradev [Task: criação do projeto - spring initializer]

---
+++ src/test/java/org/generation/grupo2/lojafarmacia/LojafarmaciaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.grupo2.lojafarmacia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojafarmaciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
