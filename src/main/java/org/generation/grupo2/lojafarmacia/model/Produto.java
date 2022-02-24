c0634c25c7a97a41b4452d44262066b28fd87470
Pedro-Marcos1223 <pedromarcospdm@gmail.com>
2022-02-24 13:47:26 -0300

    @Pedro-Marcos1223 [Task: add Model Produto]

--- src/main/java/org/generation/grupo2/lojafarmacia/model/Produto.java
+++ src/main/java/org/generation/grupo2/lojafarmacia/model/Produto.java
@@ -1,5 +1,73 @@
 package org.generation.grupo2.lojafarmacia.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produto")
 public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Size(min = 1, max = 250)
+    private String name;
+
+    @NotNull
+    private int quantidade;
+
+    @NotNull
+    private double preco;
+
+    @NotNull
+    @Size(min = 1, max = 250)
+    private String fornecedor;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public String getFornecedor() {
+        return fornecedor;
+    }
+
+    public void setFornecedor(String fornecedor) {
+        this.fornecedor = fornecedor;
+    }
 
 }
